969ce59c8cd4bac51f868886f1b840cd5e2b55b0
Colin <73126555+Co1002@users.noreply.github.com>
2023-11-27 13:54:05 +0000

    Create Game

---
+++ script.js
@@ -0,0 +1,25 @@
+const vorrat = [{image: '1', turned: false},{image: '2', turned: false},{image: '3', turned: false},{image: '4', turned: false}];
+var turnsTotal = 0;
+
+function html2element(text){
+    let template = document.createElement('template');
+    template.innerHTML = text;
+    return template.content.firstChild;
+}
+
+function createGame(){
+    vorrat = [{image: '1', turned: false},{image: '2', turned: false},{image: '3', turned: false},{image: '4', turned: false}];
+    turnsTotal = 0;
+    let karten = [...vorrat, ...vorrat];
+    let matrix = document.getElementById('matrix');
+    karten.forEach((value, index) => {
+        // HTML der Karte
+        let cardHtml = '<div class="card"><div class="card-front">'+value.image+'</div><div class="card-back"></div></div>';
+        // Aus HTML ein Element erstellen
+        let cardElement = html2element(cardHtml);
+        // Karte in die Matrix schreiben
+        matrix.appendChild(cardElement);
+    });
+}
+
+createGame();
